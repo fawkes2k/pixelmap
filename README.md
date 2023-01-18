0aeb3688234360a27196c22f2aaf1a23a64f4779
Dominik Foks <93789489+fawkes2k@users.noreply.github.com>
2023-01-18 02:08:07 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+Creates a pixel map in the spreadsheet from a picture.
+
+Example:
+```
+>>> from pixelmap import generate_pixelmap
+>>> generate_pixelmap('picture.jpg', 300)
+```
--- pixelmap.py
+++ pixelmap.py
@@ -1,8 +1,14 @@
-def generate_pixelmap(filename: str, height_of_pixelmap: int):
+def generate_pixelmap(filename: str, maximum_rows: int):
+    """
+    Creates a pixel map in the spreadsheet from a picture.
+    :param filename: Relative or absolute path to the picture.
+    :param maximum_rows: The maximum amount of rows in the spreadsheet.
+    :return: Creates a "pxmap.csv" file in the current directory.
+    """
     pixelmap_file = open('pxmap.csv', 'w')
     from wand.image import Image
     with Image(filename=filename) as image:
-        image.transform(resize=f'x{height_of_pixelmap}')
+        image.transform(resize=f'x{maximum_rows}')
         pixelmap = Image.export_pixels(image, channel_map='I')
         for i in range(len(pixelmap)):
             seperator = '\n' if i % image.width == (image.width - 1) else '\t'
